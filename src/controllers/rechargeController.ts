4f729b8ddd1ac8a6146813b4bd7d6e573a3a33bc
starunz <busouza01@gmail.com>
2022-04-19 00:06:58 -0500

    recharge completed

--- src/controllers/rechargeController.ts
+++ src/controllers/rechargeController.ts
@@ -5,31 +5,36 @@ import * as errorTypes from "../error/errorTypes.js";
 import * as existingCard from "../utils/existingCardUtils";
 import * as checkingEmployee from "../utils/checkingEmployeeUtils.js";
 import * as expirationDate from "../utils/expirationDate.js";
+import * as rechargeServices from "../services/rechargeServices.js";
 
 export async function recharge(req: Request, res: Response) {
-    const { amount } = req.body;
-    const { cardId } = req.params;
-    const { company } = res.locals;
+  const { amount } = req.body;
+  const { cardId } = req.params;
+  const { company } = res.locals;
 
-    validateRechargeAmount(req.body);
+  validateRechargeAmount(req.body);
 
-    const card = await existingCard.checkingCardExistence(
-      Number(cardId)
-    );
-  
-    await checkingEmployee.checkingEmployee(
-      card.employeeId,
-      company.id
-    );
+  const card = await existingCard.checkingCardExistence(
+    Number(cardId)
+  );
 
-    expirationDate.checkingExpirationDate(card.expirationDate);
+  await checkingEmployee.checkingEmployee(
+    card.employeeId,
+    company.id
+  );
+
+  expirationDate.checkingExpirationDate(card.expirationDate);
+
+  await rechargeServices.rechargeCard(card.id, Number(amount));
+
+  res.sendStatus(201);
 }
 
 function validateRechargeAmount(body: any) {
-    const validation = rechargeSchema.validate(body);
-  
-    if (validation.error)
-      throw errorTypes.UnprocessableEntity(
-        "A quantia de ser um valor inteiro acima de $0."
-      );
+  const validation = rechargeSchema.validate(body);
+
+  if (validation.error)
+    throw errorTypes.UnprocessableEntity(
+      "A quantia de ser um valor inteiro acima de $0."
+    );
 }
---
+++ src/services/rechargeServices.ts
@@ -0,0 +1,6 @@
+import * as rechargeRepository from "../repositories/rechargeRepository.js";
+
+export async function rechargeCard(cardId: number, amount: number) {
+
+    await rechargeRepository.insert({ cardId, amount });
+}
