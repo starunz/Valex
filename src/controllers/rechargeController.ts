914e2c4a8e6620fd95d383c1e843d18230e4ba8e
starunz <busouza01@gmail.com>
2022-04-18 23:56:57 -0500

    checked if employee and card exist

--- src/controllers/rechargeController.ts
+++ src/controllers/rechargeController.ts
@@ -2,11 +2,24 @@ import { Request, Response } from "express";
 
 import rechargeSchema from "../schemas/rechargeSchema.js";
 import * as errorTypes from "../error/errorTypes.js";
+import * as existingCard from "../utils/existingCardUtils";
+import * as checkingEmployee from "../utils/checkingEmployeeUtils.js";
 
 export async function recharge(req: Request, res: Response) {
     const { amount } = req.body;
+    const { cardId } = req.params;
+    const { company } = res.locals;
 
     validateRechargeAmount(req.body);
+
+    const card = await existingCard.checkingCardExistence(
+      Number(cardId)
+    );
+  
+    await checkingEmployee.checkingEmployee(
+      card.employeeId,
+      company.id
+    );
 }
 
 function validateRechargeAmount(body: any) {
