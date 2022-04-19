24565c7c87be970db985fec462f379e74ad16c4b
starunz <busouza01@gmail.com>
2022-04-19 00:24:13 -0500

    card balance

--- src/controllers/transactionsController.ts
+++ src/controllers/transactionsController.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
 
 import * as existingCard from "../utils/existingCardUtils.js";
+import * as cardBalance from "../utils/cardBalance.js";
 
 export async function transactions(req: Request, res: Response) {
     const { cardId } = req.params;
   
     await existingCard.checkingCardExistence(Number(cardId));
+
+    const result = await cardBalance.getBalance(Number(cardId));
+
+    res.status(200).send(result);
 }
