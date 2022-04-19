6cbac421803e43586735d91ed8dcd7c836901a80
starunz <busouza01@gmail.com>
2022-04-19 00:46:16 -0500

    validate purchase

--- src/routers/transactionsRouter.ts
+++ src/routers/transactionsRouter.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import * as rechargeController from "../controllers/rechargeController.js"
 import * as transactionsController from "../controllers/transactionsController.js";
+import * as purchaseController from "../controllers/purchaseController.js"
 import { validateApiKeyMiddleware } from "../middlewares/validateApiKeyMiddleware.js";
 
 const transactionsRouter = Router();
@@ -11,10 +12,13 @@ transactionsRouter.post("/transactions/:cardId/recharge",
     rechargeController.recharge
 );
 
-transactionsRouter.get(
-    "/transactions/:cardId/panoramic",
+transactionsRouter.get("/transactions/:cardId/panoramic",
     validateApiKeyMiddleware,
     transactionsController.transactions
+);
+
+transactionsRouter.post("/transactions/:cardId/purchase",
+    purchaseController.purchase
 );
 
 export default transactionsRouter;
