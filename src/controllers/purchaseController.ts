d1fa3f36c1974f8458e4c6acbbdc82b831a05d73
starunz <busouza01@gmail.com>
2022-04-19 00:49:46 -0500

    checked if the card exists and if it has expired

--- src/controllers/purchaseController.ts
+++ src/controllers/purchaseController.ts
@@ -2,11 +2,20 @@ import { Request, Response } from "express";
 
 import purchaseSchema from "../schemas/purchaseSchema.js";
 import * as errorTypes from "../error/errorTypes.js";
+import * as existingCard from "../utils/existingCardUtils.js";
+import * as expirationDate from "../utils/expirationDateUtils.js";
 
 export async function purchase(req: Request, res: Response) {
     const { amount, businessId, password } = req.body;
+    const { cardId } = req.params;
   
     validatePurchase(req.body);
+
+    const card = await existingCard.checkingCardExistence(
+        Number(cardId)
+      );
+    
+      expirationDate.checkingExpirationDate(card.expirationDate);
 }
 
 function validatePurchase(body: any) {
